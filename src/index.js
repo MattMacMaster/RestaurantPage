e05c27e1b144b041479e31e4c40c002567981217
Mort09 <matthew.macmaster@routeique.com>
2021-06-15 16:56:57 -0600

    Main Page done lookking into other pages

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import css from "./style.css";
-console.log(css);
+const navButtons = [...document.querySelectorAll('[data-val]')];
+navButtons.forEach((button) => {
+    button.addEventListener('click', changeSelect);
+});
+function changeSelect() {
+    if (![...this.classList].includes('menutabActive')) {
+        navButtons.forEach((button) => {
+            button.classList.remove('menutabActive');
+        });
+        this.classList.add('menutabActive');
+    };
+};
